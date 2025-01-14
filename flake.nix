69de2b848c09b342781005d0322c04ecce86666e
robertsolorzano <robertsolorzano510@gmail.com>
2025-01-13 21:11:44 -0800

    Set up a portable JupyterLab env with Nix flake
    - Created a Nix flake to set up a Python development environment with JupyterLab
    - Included necessary dependencies like Node.js, Python packages (numpy, pandas, matplotlib), and JupyterLab
    - Added shell hook with info

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A portable environment for running JupyterLab, ideal for data analysis and research.";
+
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: 
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    {
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_22
+          pkgs.python311
+          pkgs.python311Packages.numpy
+          pkgs.python311Packages.pandas
+          pkgs.python311Packages.matplotlib
+          pkgs.python311Packages.jupyterlab
+        ];
+
+      shellHook = ''
+        echo "========================================================="
+        echo "              Welcome to your JupyterLab"
+        echo "               development environment"
+        echo "---------------------------------------------------------"
+        echo "         To start the JupyterLab server, type:"
+        echo "                     jupyter-lab"
+        echo "---------------------------------------------------------"
+        echo "     Access JupyterLab at: http://localhost:8888/lab"
+        echo "     Use the browser to shut down the server cleanly"
+        echo "========================================================="
+      '';
+
+      };
+
+      defaultApp.${system} = {
+        type = "app";
+        program = "${pkgs.python311Packages.jupyterlab}/bin/jupyter-lab";
+      };
+    };
+}
